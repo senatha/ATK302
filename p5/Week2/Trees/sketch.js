0b0069edf8417f8fd133bb36cbb838543cdbf57e
senatha <55988928+senatha@users.noreply.github.com>
2020-01-23 09:14:20 -0600

    Update sketch.js

--- p5/Week2/Trees/sketch.js
+++ p5/Week2/Trees/sketch.js
@@ -1,30 +1,39 @@
 
 
-function draw(720, 400) {
-  // put drawing code here
-}
-
 function setup() {
   // Sets the screen to be 720 pixels wide and 400 pixels high
   createCanvas(720, 400);
-  background(0);
+}
+
+
+
+function draw() {
+  // put drawing code here
+  background('cyan');
   noStroke();
+  //
+  // fill(204);
+  // triangle(18, 18, 18, 360, 81, 360);
+  //
+  fill('green');
+  rect(330, 350 , 450, 500);
+  //
+  // fill(204);
+  // quad(189, 18, 216, 18, 216, 360, 144, 360);
+  //
+  fill('yellow');
+  ellipse(613, 39 , 150, 150);
+  //
+  // fill(204);
+  // triangle(288, 18, 351, 360, 288, 360);
+  //
+  // fill();
+  // arc(479, 300, 280, 280, PI, TWO_PI);
 
-  fill(204);
-  triangle(18, 18, 18, 360, 81, 360);
+fill(0) ;
+textSize (40) ;
+text(mouseX + "," + mouseY, 40, 40 ) ;
 
-  fill(102);
-  rect(81, 81, 63, 63);
 
-  fill(204);
-  quad(189, 18, 216, 18, 216, 360, 144, 360);
 
-  fill(255);
-  ellipse(252, 144, 72, 72);
-
-  fill(204);
-  triangle(288, 18, 351, 360, 288, 360);
-
-  fill(255);
-  arc(479, 300, 280, 280, PI, TWO_PI);
 }
