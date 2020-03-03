f84312c18c5310c9af9e6e5b3087fa4d881052a1
senatha <55988928+senatha@users.noreply.github.com>
2020-03-03 09:33:29 -0600

    chnage

---
+++ p5/Week7/stoplight/sketch.js
@@ -0,0 +1,50 @@
+var myState = 0 ;
+var timer = 0 ;
+
+
+function setup() {
+  createCanvas(800, 800) ;
+
+  rectMode('center') ;
+  ellipse('center') ;
+}
+
+function draw() {
+  fill('black')
+  rect(width/2, height/2, 200, 500) ;
+
+timer = 1+timer ;
+if (timer = 60)
+
+myState = myState + 1 ;
+timer = 0 ;
+}
+
+if(myState = 3)
+
+myState = 0 ;
+
+
+
+switch (myState) {
+  
+  case 0:
+  fill(97, 255, 69, 100)
+  ellipse(width/2, height/2, 150, 150)
+  console.log("green light");
+  break;
+
+  case 1:
+  fill(255,234,67,100)
+  ellipse(width/2, height/2 - 160, 150, 150);
+  console.log('yellow light');
+
+  case 2:
+  fill(255,34,26,100)
+  ellipse(width/2, height/2 + 160, 150, 150);
+  console.log('red light');
+
+ break;
+
+}
+}
