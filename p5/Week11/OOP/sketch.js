b69f2d84ca83793656f3767b90bac4765ae94013
senatha <55988928+senatha@users.noreply.github.com>
2020-03-29 14:15:37 -0600

    Update sketch.js

--- p5/Week11/OOP/sketch.js
+++ p5/Week11/OOP/sketch.js
@@ -1,54 +1,53 @@
-var myCar;
-var myCar2;
+var cars = [];
 
 function setup() {
-  // put setup code here
   createCanvas(800, 800);
-  myCar = new Car();
-  myCar2 = new Car();
 
+  for (var i = 0; i < 20; i++) {
+    cars.push(new Car());
+
+  }
 }
 
 
 function draw() {
-
   background('lightblue');
 
-  myCar.display();
-  myCar.drive();
-
-  myCar2.display();
-  myCar2.drive();
-
-}
-
-//car class
-function Car() {
-  //attributes
-  this.pos = createVector(random(width), random(height));
-  this.vel = createVector(random(-5, 5), random(-5,5)) ;
-  this.r = random(255) ;
-  this.g = random(255) ;
-  this.b = random(255) ;
-
-  //methods
-
-  this.display = function() {
-    fill(this.r, this.g, this.b) ;
-    rect(this.pos.x, this.pos.y, 100, 100);
+  for (var i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].drive();
 
   }
+}
 
-  this.drive = function() {
-    this.pos.add(this.vel);
-    if (this.pos.x > width) this.pos.x = 0 ;
-    if (this.pos.x < 0) this.pos.x = width ;
-    if (this.pos.y > height) this.pos.y = 0 ;
-    if (this.pos.y < 0) this.pos.y = height ;
+  //car class
+  function Car() {
+    //attributes
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(random(-5, 5), random(-5, 5));
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+
+    //methods
+
+    this.display = function() {
+      fill(this.r, this.g, this.b);
+      rect(this.pos.x, this.pos.y, 100, 100);
+
+    }
+
+    this.drive = function() {
+      this.pos.add(this.vel);
+      if (this.pos.x > width) this.pos.x = 0;
+      if (this.pos.x < 0) this.pos.x = width;
+      if (this.pos.y > height) this.pos.y = 0;
+      if (this.pos.y < 0) this.pos.y = height;
 
     }
     //code phrase - city you were born
     //favorite movie character of all time
-    //favorite hobby
+    //favorite
+    //favorite food
   }
 }
