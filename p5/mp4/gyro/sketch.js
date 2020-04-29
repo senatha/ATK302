a74291f9b0d124c893b829ff4cc181ed4ab0001c
senatha <55988928+senatha@users.noreply.github.com>
2020-04-28 21:01:50 -0600

    fgbn

--- p5/mp4/gyro/sketch.js
+++ p5/mp4/gyro/sketch.js
@@ -65,7 +65,7 @@ function draw() {
   // MORE DECORATIONS - write that pretty ATK type on top.
   fill('white');
   noStroke();
-  textSize(200);
+  textSize(100);
   textAlign(CENTER);
   text("Hi!", width / 2, height / 2);
 
