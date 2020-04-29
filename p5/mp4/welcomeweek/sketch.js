5c0101e2aee04467348d0666e35f9d24776a95ef
senatha <55988928+senatha@users.noreply.github.com>
2020-04-28 23:01:35 -0600

    fghj

--- p5/mp4/welcomeweek/sketch.js
+++ p5/mp4/welcomeweek/sketch.js
@@ -20,7 +20,7 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   num = 0;
   intervalCurrentPosition(positionPing, 5000);
-  places.push(new Place(40.47859881213726, -88.96815846900026, "Rose's Room", .02, reggieImg));
+  
   places.push(new Place(41.612728, -87.588538572, "Shaz's Room", .02, reggieImg)); // new Place object, for CVA room 17
 
   places.push(new Place(40.50622797365503, -88.99051350503431, "CVA 17", .02, reggieImg)); // new Place object, for CVA room 17
