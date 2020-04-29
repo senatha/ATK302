6c7a5cb56524e67b35bc6f96c56667d64a8ce74e
senatha <55988928+senatha@users.noreply.github.com>
2020-04-28 18:30:45 -0600

    20

--- p5/mp4/gyro/sketch.js
+++ p5/mp4/gyro/sketch.js
@@ -65,7 +65,7 @@ function draw() {
   // MORE DECORATIONS - write that pretty ATK type on top.
   fill('white');
   noStroke();
-  textSize(100);
+  textSize(20);
   textAlign(CENTER);
   text("STAY HOME", width / 2, height / 2);
 
